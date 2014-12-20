06e80910684deca03bd4a209d6bf0e5c22c8f65a
Jochen Gaiser <m@jojen.de>
2014-12-20 02:47:54 +0200

    Display

--- pbox/main.py
+++ pbox/main.py
@@ -1,17 +1,43 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 import RPi.GPIO as GPIO
 import time
+from Adafruit_CharLCD import Adafruit_CharLCD
 
-LED = 11
+lcd = Adafruit_CharLCD()
 
-GPIO.setmode(GPIO.BOARD)
-# need to set up every channel which are using as an input or an output  
+while 1:
+    lcd.clear()
+    lcd.begin(16,1)
+    lcd.message('huhu\n')
+    lcd.message('huhu2')
+    time.sleep(20)
+#LED_GPIO = 17
+#B1_GPIO = 23
 
-GPIO.setup(LED, GPIO.OUT)
 
+
+#GPIO.setmode(GPIO.BCM)
+
+#GPIO.setup(LED_GPIO, GPIO.OUT)
+#GPIO.setup(B1_GPIO, GPIO.IN)
+#led_status = GPIO.input(LED_GPIO)
+
+"""
 while True:
-    GPIO.output(LED, GPIO.HIGH)
-    time.sleep(2)
-    GPIO.output(LED, GPIO.LOW)
-    time.sleep(2)
+    try:
+        b1_state = GPIO.input(B1_GPIO)
+        if b1_state == True:
+            print ("huhu")
+            if led_status == False:
+                GPIO.output(LED_GPIO, GPIO.HIGH)
+                led_status = True
+            else:
+                GPIO.output(LED_GPIO, GPIO.LOW)
+                led_status = False
+            time.sleep(0.2)
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        exit()
+"""
